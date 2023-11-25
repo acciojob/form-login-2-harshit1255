2972a679a557bb0b6e04d721ec94998b1e0754e6
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-26 00:14:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	alert(`First Name: ${form.fname.value}\nLast Name: ${form.lname.value}\nPhone Number: ${form.number.value}\nEmail ID: ${form.email.value}`)
+})
